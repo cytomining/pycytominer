cd9e4b90e693f57172c9cc1ed4add851b25049e7
gwaygenomics <gregory.way@gmail.com>
2019-07-03 16:47:04 -0400

    add cytominer authors to setup

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
+"""
+Functionality in pycytominer is modified from https://github.com/cytomining/cytominer
+
+The original authors of the cytominer package are:
+Tim Becker, Allen Goodman, Claire McQuin, Mohammad Rohban, and Shantanu Singh
+"""
+
 from setuptools import setup
 from setuptools import find_packages
 
 setup(
     name="pycytominer",
     description="package for processing and mining morphological profiles",
-    long_description="package to normalize, manipulate, and process morphological profiling data",
-    author="Gregory Way",
-    author_email="gregory.way@gmail.com",
+    long_description="package to normalize, manipulate, and process morphological profiling data. The python implementation of cytominer.",
+    maintainer="Gregory Way",
+    maintainer_email="gregory.way@gmail.com",
     url="https://github.com/cytomining/pycytominer",
     packages=find_packages(),
     license="BSD 3-Clause License",
