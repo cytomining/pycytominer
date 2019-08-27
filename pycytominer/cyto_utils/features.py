c6d2423180dd05ad8798bc7f9c27d9a677755ae4
gwaygenomics <gregory.way@gmail.com>
2019-08-27 17:28:02 -0400

    add infer_cp_feature function
    also adding label_compartment function

--- pycytominer/cyto_utils/features.py
+++ pycytominer/cyto_utils/features.py
@@ -34,3 +34,50 @@ def get_blacklist_features(blacklist_file=blacklist_file, population_df=None):
         blacklist_features = [x for x in blacklist_features if x in population_features]
 
     return blacklist_features
+
+
+def label_compartment(cp_features, compartment, metadata_cols):
+    """
+    Assign compartment label to each features as a prefix
+
+    Arguments:
+    cp_features - list of all features being used
+    compartment - a string indicating the measured compartment
+    metadata_cols - a list indicating which columns should be considered metadata
+
+    Return:
+    Recoded column names with appopriate metadata and compartment labels
+    """
+
+    compartment = compartment.Title()
+    avail_compartments = ["Cells", "Cytoplasm", "Nuceli", "Image", "Barcode"]
+
+    assert (
+        compartment in avail_compartments
+    ), "provide valid compartment. One of: {}".format(avail_compartments)
+
+    cp_features = [
+        "Metadata_{}".format(x)
+        if x in metadata_cols
+        else "{}_{}".format(compartment, x)
+        for x in cp_features
+    ]
+
+    return cp_features
+
+
+def infer_cp_features(population_df):
+    """
+    Given a dataframe, output features that we expect to be cell painting features
+    """
+    features = [
+        x
+        for x in population_df.columns.tolist()
+        if (
+            x.startswith("Cells_")
+            | x.startswith("Nuclei_")
+            | x.startswith("Cytoplasm_")
+        )
+    ]
+
+    return features
