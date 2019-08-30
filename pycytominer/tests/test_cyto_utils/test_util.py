c5b4275915ecb685a4ebdcd04c681097990bf472
gwaygenomics <gregory.way@gmail.com>
2019-08-30 16:09:47 -0400

    add test for loading known metadata dictionary

--- pycytominer/tests/test_cyto_utils/test_util.py
+++ pycytominer/tests/test_cyto_utils/test_util.py
@@ -1,8 +1,14 @@
 import os
 import random
 import pytest
+import tempfile
 import pandas as pd
-from pycytominer.cyto_utils.util import check_compartments
+from pycytominer.cyto_utils.util import (
+    check_compartments,
+    load_known_metadata_dictionary,
+)
+
+tmpdir = tempfile.gettempdir()
 
 
 def test_check_compartments():
@@ -40,3 +46,25 @@ def test_check_compartments_not_valid():
         not_valid = ["Cells", "Cytoplasm", "SOMETHING"]
         output = check_compartments(not_valid)
     assert "compartment not supported" in str(ae.value)
+
+
+def test_load_known_metadata_dictionary():
+    meta_cols = ["ObjectNumber", "ImageNumber", "TableNumber"]
+    meta_df = pd.DataFrame(
+        {
+            "compartment": ["cells"] * 3 + ["nuclei"] * 3 + ["cytoplasm"] * 3,
+            "feature": meta_cols * 3,
+        }
+    )
+
+    metadata_file = os.path.join(tmpdir, "metadata_temp.txt")
+    meta_df.to_csv(metadata_file, sep="\t", index=False)
+
+    result = load_known_metadata_dictionary(metadata_file)
+
+    expected_result = {
+        "cells": meta_cols,
+        "nuclei": meta_cols,
+        "cytoplasm": meta_cols,
+    }
+    assert result == expected_result
