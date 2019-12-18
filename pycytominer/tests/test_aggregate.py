734e3d23f8078c496e24e873bb0aa13636cad8fd
gwaygenomics <gregory.way@gmail.com>
2019-12-18 10:22:30 -0500

    add test for data with missing values

--- pycytominer/tests/test_aggregate.py
+++ pycytominer/tests/test_aggregate.py
@@ -1,13 +1,20 @@
+import numpy as np
 import pandas as pd
 from pycytominer import aggregate
 from pycytominer.cyto_utils import infer_cp_features
-
 
 # Build data to use in tests
 data_df = pd.concat(
     [
         pd.DataFrame({"g": "a", "Cells_x": [1, 3, 8], "Nuclei_y": [5, 3, 1]}),
         pd.DataFrame({"g": "b", "Cells_x": [1, 3, 5], "Nuclei_y": [8, 3, 1]}),
+    ]
+).reset_index(drop=True)
+
+data_missing_df = pd.concat(
+    [
+        pd.DataFrame({"g": "a", "Cells_x": [1, 3, 8, np.nan], "Nuclei_y": [5, np.nan, 3, 1]}),
+        pd.DataFrame({"g": "b", "Cells_x": [1, 3, np.nan, 5], "Nuclei_y": [np.nan, 8, 3, 1]}),
     ]
 ).reset_index(drop=True)
 
@@ -103,3 +110,28 @@ def test_aggregate_median_dtype_confirm():
     expected_result = expected_result.astype(dtype_convert_dict)
 
     assert aggregate_result.equals(expected_result)
+
+
+def test_aggregate_median_with_missing_values():
+    """
+    Testing aggregate pycytominer function
+    """
+
+    # Convert dtype of one variable to object
+    data_dtype_df = data_missing_df.copy()
+    data_dtype_df.Cells_x = data_dtype_df.Cells_x.astype(str)
+
+    aggregate_result = aggregate(
+        population_df=data_dtype_df, strata=["g"], features="infer", operation="median"
+    )
+    print(aggregate_result)
+    expected_result = pd.concat(
+        [
+            pd.DataFrame({"g": "a", "Cells_x": [3], "Nuclei_y": [3]}),
+            pd.DataFrame({"g": "b", "Cells_x": [3], "Nuclei_y": [3]}),
+        ]
+    ).reset_index(drop=True)
+    expected_result = expected_result.astype(dtype_convert_dict)
+
+    assert aggregate_result.equals(expected_result)
+
