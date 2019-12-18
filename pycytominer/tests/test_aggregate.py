6ac568c20ce0dfb562203f5fef10891dd1be291d
gwaygenomics <gregory.way@gmail.com>
2019-12-18 10:14:10 -0500

    manage dtypes in aggregate_test

--- pycytominer/tests/test_aggregate.py
+++ pycytominer/tests/test_aggregate.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from pycytominer import aggregate
+from pycytominer.cyto_utils import infer_cp_features
+
 
 # Build data to use in tests
 data_df = pd.concat(
@@ -8,6 +10,9 @@ data_df = pd.concat(
         pd.DataFrame({"g": "b", "Cells_x": [1, 3, 5], "Nuclei_y": [8, 3, 1]}),
     ]
 ).reset_index(drop=True)
+
+features = infer_cp_features(data_df)
+dtype_convert_dict = {x: float for x in features}
 
 
 def test_aggregate_median_allvar():
@@ -24,6 +29,7 @@ def test_aggregate_median_allvar():
             pd.DataFrame({"g": "b", "Cells_x": [3], "Nuclei_y": [3]}),
         ]
     ).reset_index(drop=True)
+    expected_result = expected_result.astype(dtype_convert_dict)
 
     assert aggregate_result.equals(expected_result)
 
@@ -42,6 +48,7 @@ def test_aggregate_mean_allvar():
             pd.DataFrame({"g": "b", "Cells_x": [3], "Nuclei_y": [4]}),
         ]
     ).reset_index(drop=True)
+    expected_result = expected_result.astype(dtype_convert_dict)
 
     assert aggregate_result.equals(expected_result)
 
@@ -55,6 +62,7 @@ def test_aggregate_median_subsetvar():
     )
 
     expected_result = pd.DataFrame({"g": ["a", "b"], "Cells_x": [3, 3]})
+    expected_result.Cells_x = expected_result.Cells_x.astype(float)
 
     assert aggregate_result.equals(expected_result)
 
@@ -68,6 +76,7 @@ def test_aggregate_mean_subsetvar():
     )
 
     expected_result = pd.DataFrame({"g": ["a", "b"], "Cells_x": [4, 3]})
+    expected_result.Cells_x = expected_result.Cells_x.astype(float)
 
     assert aggregate_result.equals(expected_result)
 
@@ -91,5 +100,6 @@ def test_aggregate_median_dtype_confirm():
             pd.DataFrame({"g": "b", "Cells_x": [3], "Nuclei_y": [3]}),
         ]
     ).reset_index(drop=True)
+    expected_result = expected_result.astype(dtype_convert_dict)
 
     assert aggregate_result.equals(expected_result)
