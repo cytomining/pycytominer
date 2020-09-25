3e1017bb32275002a48eefaf0ac9ba62675297c3
gwaygenomics <gregory.way@gmail.com>
2020-09-25 17:00:04 -0400

    update init to import spherize instead of whiten

--- pycytominer/operations/__init__.py
+++ pycytominer/operations/__init__.py
@@ -1,5 +1,5 @@
 from .correlation_threshold import correlation_threshold
 from .variance_threshold import variance_threshold, calculate_frequency
 from .get_na_columns import get_na_columns
-from .transform import Whiten, RobustMAD
+from .transform import Spherize, RobustMAD
 from .sparse_random_projection import sparse_random_projection
