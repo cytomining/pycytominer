ff8e2f15fca64a721af2fd9db522291296c35394
gwaygenomics <gregory.way@gmail.com>
2020-10-09 10:56:54 -0400

    update conf to include theme option and add coverage

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ file_loc = os.path.split(__file__)[0]
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(file_loc), ".")))
 
 import pycytominer
-
+import groundwork_sphinx_theme
 
 # -- Project information -----------------------------------------------------
 project = pycytominer.__about__.__project__
@@ -38,7 +38,13 @@ release = version
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["autoapi.extension"]
+extensions = [
+    "autoapi.extension",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.coverage",
+]
 autoapi_dirs = ["../pycytominer"]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,7 +53,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -55,7 +61,16 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "groundwork-sphinx-theme"
+html_theme = "groundwork"
+
+html_theme_options = {
+    "sidebar_width": "240px",
+    "stickysidebar": True,
+    "stickysidebarscrollable": True,
+    "contribute": True,
+    "github_fork": "useblocks/groundwork",
+    "github_user": "useblocks",
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
