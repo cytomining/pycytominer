ba211b1d3aff7d816072f87091144e963c57488d
gwaygenomics <gregory.way@gmail.com>
2020-10-09 10:56:33 -0400

    set and install theme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import pathlib
 from setuptools import setup
 from setuptools import find_packages
 
-with open('README.md', encoding="utf-8") as readme_file:
+with open("README.md", encoding="utf-8") as readme_file:
     long_description = readme_file.read()
 
 about = {}
