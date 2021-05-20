17f92c6d860f6013c15a6870fbd06c6b5c857808
michaelbornholdt <michael.bornholdt@outlook.com>
2021-05-20 14:53:34 -0400

    Add first test run

--- pycytominer/tests/test_cyto_utils/test_DeepProfiler_processing.py
+++ pycytominer/tests/test_cyto_utils/test_DeepProfiler_processing.py
@@ -1,2 +1,43 @@
 import os
+import random
+import pytest
+import tempfile
 import numpy as np
+import pandas as pd
+
+import sys
+sys.path.insert(0, "../../../")
+print(os.getcwd())
+
+from pycytominer.cyto_utils import load_profiles, load_platemap, load_npz
+from pycytominer.cyto_utils.load import infer_delim
+from pycytominer.cyto_utils.DeepProfiler_processing import AggregateDeepProfiler
+
+
+example_project_dir = os.path.join(
+    os.path.dirname(__file__),
+    "..",
+    "test_data",
+    "DeepProfiler_data"
+)
+
+profile_dir = os.path.join(
+    example_project_dir,
+    "outputs",
+    "results",
+    "features"
+)
+
+index_file = os.path.join(
+    example_project_dir,
+    "inputs",
+    "metadata",
+    "test_index.csv"
+)
+
+annotate_cols = ['feat0', 'feat1']
+
+test_class = AggregateDeepProfiler(index_file=index_file, profile_dir=profile_dir, aggregate_on="well")
+
+df = test_class.annotate_deep(annotate_cols=annotate_cols)
+
