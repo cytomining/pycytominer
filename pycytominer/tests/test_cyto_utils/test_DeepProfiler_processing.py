92be40cd3325e131a68f725022f9a1ec0856b0ea
michaelbornholdt <michael.bornholdt@outlook.com>
2021-05-20 16:36:48 -0400

    Black

--- pycytominer/tests/test_cyto_utils/test_DeepProfiler_processing.py
+++ pycytominer/tests/test_cyto_utils/test_DeepProfiler_processing.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 
 import sys
+
 sys.path.insert(0, "../../../")
 print(os.getcwd())
 
@@ -15,29 +16,17 @@ from pycytominer.cyto_utils.DeepProfiler_processing import AggregateDeepProfiler
 
 
 example_project_dir = os.path.join(
-    os.path.dirname(__file__),
-    "..",
-    "test_data",
-    "DeepProfiler_example_data_2"
+    os.path.dirname(__file__), "..", "test_data", "DeepProfiler_example_data_2"
 )
 
-profile_dir = os.path.join(
-    example_project_dir,
-    "outputs",
-    "results",
-    "features"
+profile_dir = os.path.join(example_project_dir, "outputs", "results", "features")
+
+index_file = os.path.join(example_project_dir, "inputs", "metadata", "test_index.csv")
+
+annotate_cols = ["feat0", "feat1"]
+
+test_class = AggregateDeepProfiler(
+    index_file=index_file, profile_dir=profile_dir, aggregate_on="well"
 )
 
-index_file = os.path.join(
-    example_project_dir,
-    "inputs",
-    "metadata",
-    "test_index.csv"
-)
-
-annotate_cols = ['feat0', 'feat1']
-
-test_class = AggregateDeepProfiler(index_file=index_file, profile_dir=profile_dir, aggregate_on="well")
-
-df = test_class.annotate_deep(annotate_cols=annotate_cols)
-
+df = test_class.annotate_deep()
