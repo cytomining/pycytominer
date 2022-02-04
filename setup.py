8b3a572b5f3811d07241f6727581ba3b7a8184da
Steve Taylor <staylorx@gmail.com>
2022-02-04 16:57:33 -0700

    Add first cut on publishing to pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 import pathlib
+import os
 from setuptools import setup
 from setuptools import find_packages
 
 with open("README.md", encoding="utf-8") as readme_file:
     long_description = readme_file.read()
+
+# The directory containing this file
+my_current_dir = pathlib.Path(__file__).parent
+
+# Pull requirements from the text file
+requirement_path = os.path.join(my_current_dir, "requirements.txt")
+
+install_requires = []
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
+
+testing_requires = []
+install_requires = install_requires + testing_requires
 
 about = {}
 with open(pathlib.Path("pycytominer/__about__.py")) as fp:
@@ -20,7 +35,7 @@ setup(
     url="https://github.com/cytomining/pycytominer",
     packages=find_packages(),
     license=about["__license__"],
-    install_requires=["numpy", "pandas", "scikit-learn", "sqlalchemy"],
+    install_requires=install_requires,
     python_requires=">=3.4",
     include_package_data=True,
 )
