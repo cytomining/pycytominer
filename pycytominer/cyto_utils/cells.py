f4c8889bebbdf51fec358c0efba6d0bff5af40a8
Gregory Way <gregory.way@gmail.com>
2022-10-18 15:03:45 -0600

    add image_loaded attribute

--- pycytominer/cyto_utils/cells.py
+++ pycytominer/cyto_utils/cells.py
@@ -163,8 +163,10 @@ class SingleCells(object):
         # Confirm that the input fields of view is valid
         self.fields_of_view = check_fields_of_view_format(fields_of_view)
 
+        self.image_data_loaded = False
         if self.load_image_data:
             self.load_image(image_table_name=self.image_table_name)
+            self.image_data_loaded = True
 
     def _check_subsampling(self):
         """Internal method checking if subsampling options were specified correctly.
@@ -286,7 +288,7 @@ class SingleCells(object):
                     f"{self.fields_of_view_feature}==@self.fields_of_view"
                 )
 
-        self.load_image_data = True
+        self.image_data_loaded = True
 
     def count_cells(self, compartment="cells", count_subset=False):
         """Determine how many cells are measured per well.
@@ -502,9 +504,9 @@ class SingleCells(object):
             self.get_subsample(compartment=compartment)
 
         # Load image data if not already loaded
-        if not self.load_image_data:
+        if not self.image_data_loaded:
             self.load_image(image_table_name=self.image_table_name)
-            self.load_image_data = True
+            self.image_data_loaded = True
 
         # Iteratively call aggregate() on chunks of the full compartment table
         object_dfs = []
@@ -761,9 +763,9 @@ class SingleCells(object):
         )
 
         # Add image data to single cell dataframe
-        if not self.load_image_data:
+        if not self.image_data_loaded:
             self.load_image(image_table_name=self.image_table_name)
-            self.load_image_data = True
+            self.image_data_loaded = True
 
         sc_df = (
             self.image_df.merge(sc_df, on=self.merge_cols, how="right")
