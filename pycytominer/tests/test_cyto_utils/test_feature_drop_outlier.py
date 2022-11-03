87f80bfbca7638b97180387815687b5604104f93
bethac07 <bcimini@broadinstitute.org>
2022-11-03 17:00:43 -0400

    Fix failing tests, add one

--- pycytominer/tests/test_cyto_utils/test_feature_drop_outlier.py
+++ pycytominer/tests/test_cyto_utils/test_feature_drop_outlier.py
@@ -23,29 +23,34 @@ data_df = pd.DataFrame(
         "Cells_x": [1, 2, -8, 2, 5, 5, 5, -1],
         "Cytoplasm_y": [3, -1, 7, 4, 5, -9, 6, 1],
         "Nuclei_z": [-1, 8, 2, 5, -6, 20, 2, -2],
-        "Cells_zz": [14, -46, 1, 60, -30, -100, 2, 2],
+        "Cells_zz": [14, -46, 1, 60, -30, -10000, 2, 2],
     }
 ).reset_index(drop=True)
 
 
 def test_outlier_default():
     result = drop_outlier_features(data_df)
-    expected_result = ["Cells_zz", "Nuclei_z"]
+    expected_result = ["Cells_zz"]
     assert sorted(result) == sorted(expected_result)
 
 
-def test_outlier_high_cutoff():
+def test_outlier_30_cutoff():
     result = drop_outlier_features(data_df, outlier_cutoff=30)
     expected_result = ["Cells_zz"]
     assert result == expected_result
 
-
-def test_outlier_samples():
-    result = drop_outlier_features(data_df, samples=[0, 1, 2, 3, 5])
+def test_outlier_15_cutoff():
+    result = drop_outlier_features(data_df, outlier_cutoff=15)
     expected_result = ["Cells_zz", "Nuclei_z"]
     assert sorted(result) == sorted(expected_result)
 
-    result = drop_outlier_features(data_df, samples=[0, 1, 2, 3])
+
+def test_outlier_samples_15():
+    result = drop_outlier_features(data_df, samples=[0, 1, 2, 3, 5], outlier_cutoff=15)
+    expected_result = ["Cells_zz", "Nuclei_z"]
+    assert sorted(result) == sorted(expected_result)
+
+    result = drop_outlier_features(data_df, samples=[0, 1, 2, 3], outlier_cutoff=15)
     expected_result = ["Cells_zz"]
     assert result == expected_result
 
