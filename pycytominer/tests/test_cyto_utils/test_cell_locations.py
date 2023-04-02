76e1f49759d0beabbdd8537bf14711f7f2d40a84
Shantanu Singh <shsingh@broadinstitute.org>
2023-04-02 07:02:01 -0400

    Address various comment

--- pycytominer/tests/test_cyto_utils/test_cell_locations.py
+++ pycytominer/tests/test_cyto_utils/test_cell_locations.py
@@ -1,15 +1,22 @@
 """This tests the output from CellLocation class"""
 
-import os
 import pandas as pd
-from pycytominer.cyto_utils.cell_locations import CellLocation
 import pytest
-import sqlite3
+import sqlalchemy
+from typing import Literal, Type
+from _pytest.fixtures import FixtureRequest
 
 
 @pytest.mark.parametrize("cell_loc", ["cell_loc1", "cell_loc2", "cell_loc3"])
-# @pytest.mark.parametrize("cell_loc", ["cell_loc1", "cell_loc2"])
-def test_shape_and_columns(cell_loc, metadata_input_dataframe, request):
+def test_output_shape_and_required_columns(
+    cell_loc: Literal["cell_loc1", "cell_loc2", "cell_loc3"],
+    metadata_input_dataframe: pd.DataFrame,
+    request: Type[FixtureRequest],
+):
+    """
+    This tests the shape of the output from CellLocation class and verifies that the required columns are present
+    """
+
     cell_loc = request.getfixturevalue(cell_loc)
 
     # check the shape of the data
@@ -24,8 +31,15 @@ def test_shape_and_columns(cell_loc, metadata_input_dataframe, request):
 
 
 @pytest.mark.parametrize("cell_loc", ["cell_loc1", "cell_loc2", "cell_loc3"])
-# @pytest.mark.parametrize("cell_loc", ["cell_loc1", "cell_loc2"])
-def test_values(cell_loc, metadata_input_dataframe, single_cell_input_file, request):
+def test_output_value_correctness(
+    cell_loc: Literal["cell_loc1", "cell_loc2", "cell_loc3"],
+    metadata_input_dataframe: pd.DataFrame,
+    single_cell_input_file: str,
+    request: Type[FixtureRequest],
+):
+    """
+    This tests the correctness of the values in the output from CellLocation class by comparing the values in the output to the values in the input
+    """
     cell_loc = request.getfixturevalue(cell_loc)
 
     # if we restrict the columns of cell_loc to the ones in metadata_input_dataframe, we should get the same dataframe
@@ -35,13 +49,11 @@ def test_values(cell_loc, metadata_input_dataframe, single_cell_input_file, requ
         .equals(metadata_input_dataframe.reset_index(drop=True))
     )
 
+    engine = sqlalchemy.create_engine(f"sqlite:///{single_cell_input_file}")
+
     nuclei_query = "SELECT ImageNumber, ObjectNumber, Nuclei_Location_Center_X, Nuclei_Location_Center_Y FROM Nuclei;"
 
-    conn = sqlite3.connect(single_cell_input_file)
-
-    nuclei_df = pd.read_sql_query(nuclei_query, conn)
-
-    conn.close()
+    nuclei_df = pd.read_sql_query(nuclei_query, engine)
 
     # get the values in the Nuclear_Location_Center_X and Nuclear_Location_Center_Y columns
     # for the rows in nuclei_df that have ImageNumber == 1
