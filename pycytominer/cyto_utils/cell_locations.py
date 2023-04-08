02f319ea5a236bad114d08d1cac58f58240f6bb0
Shantanu Singh <shsingh@broadinstitute.org>
2023-04-08 10:06:58 -0400

    bug fix

--- pycytominer/cyto_utils/cell_locations.py
+++ pycytominer/cyto_utils/cell_locations.py
@@ -147,7 +147,7 @@ class CellLocation:
         bucket, key = self._parse_s3_path(s3_path)
 
         try:
-            self.s3.Object(bucket, key).load()
+            self.s3.head_object(Bucket=bucket, Key=key)
         except botocore.exceptions.ClientError as e:
             if e.response["Error"]["Code"] == "404":
                 return False
