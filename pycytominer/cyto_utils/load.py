25f0ac86ba0a57f200455e37d755ed6e855d5a99
Gregory Way <gregory.way@gmail.com>
2023-06-04 08:36:45 -0600

    move file path resolve to test_parquet function
    this change required propagation to other files

--- pycytominer/cyto_utils/load.py
+++ pycytominer/cyto_utils/load.py
@@ -30,14 +30,12 @@ def is_path_a_parquet_file(file: Union[str, pathlib.PurePath]) -> bool:
     FileNotFoundError
         Raised if the provided path in the `file` does not exist
     """
-    # type checking
-    if not isinstance(file, (str, pathlib.PurePath)):
-        raise TypeError(f"file must be a str or pathlib.PurePath not {type(file)}")
 
-    # Convert str to pathlib.Path object and absolute path
-    # check if the file also exists while converting to absolute path
-    if isinstance(file, str):
-        file = pathlib.Path(file).resolve(strict=True)
+    file = pathlib.PurePath(file)
+    try:
+        pathlib.Path(file).resolve(strict=True)
+    except FileNotFoundError as e:
+        print("load_profiles() didn't find the path.", e, sep="\n")
 
     # Check if file path is a parquet file
     if file.suffix.lower() == ".parquet":
@@ -77,7 +75,7 @@ def load_profiles(profiles):
 
     Parameters
     ----------
-    profiles : {str, pandas.DataFrame}
+    profiles : {str, pathlib.Path, pandas.DataFrame}
         file location or actual pandas dataframe of profiles
 
     Return
@@ -94,13 +92,11 @@ def load_profiles(profiles):
         if is_path_a_parquet_file(profiles):
             return pd.read_parquet(profiles, engine="pyarrow")
 
-        try:
+        else:
             delim = infer_delim(profiles)
-            profiles = pd.read_csv(profiles, sep=delim)
-        except FileNotFoundError:
-            raise FileNotFoundError(f"{profiles} profile file not found")
+            return pd.read_csv(profiles, sep=delim)
 
-    return profiles
+    return profiles 
 
 
 def load_platemap(platemap, add_metadata_id=True):
@@ -176,12 +172,9 @@ def load_npz_features(npz_file, fallback_feature_prefix="DP", metadata=True):
     # Load metadata
     if "metadata" in files:
         metadata = npz["metadata"].item()
-        metadata_df = pd.DataFrame(
-            metadata, index=range(0, df.shape[0]), dtype=str
-        )
+        metadata_df = pd.DataFrame(metadata, index=range(0, df.shape[0]), dtype=str)
         metadata_df.columns = [
-            f"Metadata_{x}" if not x.startswith("Metadata_") else x
-            for x in metadata_df
+            f"Metadata_{x}" if not x.startswith("Metadata_") else x for x in metadata_df
         ]
 
         # Determine the appropriate metadata prefix
@@ -200,16 +193,12 @@ def load_npz_features(npz_file, fallback_feature_prefix="DP", metadata=True):
 
     # Append metadata with features
     if "metadata" in files:
-        df = metadata_df.merge(
-            df, how="outer", left_index=True, right_index=True
-        )
+        df = metadata_df.merge(df, how="outer", left_index=True, right_index=True)
 
     return df
 
 
-def load_npz_locations(
-    npz_file, location_x_col_index=0, location_y_col_index=1
-):
+def load_npz_locations(npz_file, location_x_col_index=0, location_y_col_index=1):
     """
     Load an npz file storing locations and, sometimes, metadata.
 
