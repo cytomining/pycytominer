2b11f29d95703d2782a4f6a04ae8d8f077c3488d
Gregory Way <gregory.way@gmail.com>
2023-06-11 06:34:33 -0600

    precommit

--- docs/conf.py
+++ docs/conf.py
@@ -36,12 +36,7 @@ release = version
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
-    "sphinx_copybutton",
-    "m2r2"
-]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_copybutton", "m2r2"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -56,8 +51,23 @@ exclude_patterns = ["**tests**"]
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
+pycytominer_hex = "#c24dd6"
 html_theme = "furo"
-                         
+html_theme_options = {
+    "light_css_variables": {
+        "color-brand-primary": pycytominer_hex,
+        "color-brand-content": pycytominer_hex,
+        "color-api-pre-name": pycytominer_hex,
+        "color-api-name": pycytominer_hex,
+    },
+    "dark_css_variables": {
+        "color-brand-primary": pycytominer_hex,
+        "color-brand-content": pycytominer_hex,
+        "color-api-pre-name": pycytominer_hex,
+        "color-api-name": pycytominer_hex,
+    },
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
