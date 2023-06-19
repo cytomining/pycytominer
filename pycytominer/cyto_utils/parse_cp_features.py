2189abd51330cc4665d82a108f650c145c5a1f7e
Shantanu Singh <shsingh@broadinstitute.org>
2023-06-19 11:05:34 -0400

    docs

--- pycytominer/cyto_utils/parse_cp_features.py
+++ pycytominer/cyto_utils/parse_cp_features.py
@@ -28,10 +28,22 @@ def parse_cp_features(feature):
         feature_group = "Unknown"
     else:
         if feature_group in ["AreaShape", "Neighbors", "Children", "Parent", "Number"]:
+            # Examples:
+            # Cells,AreaShape,Zernike_2_0
+            # Cells,AreaShape,BoundingBoxArea
+            # Cells,Neighbors,AngleBetweenNeighbors_Adjacent
+            # Nuclei,Children,Cytoplasm_Count
+            # Nuclei,Parent,NucleiIncludingEdges
+            # Nuclei,Number,ObjectNumber
+
             feature_type = parts[2]
             channel = "None"
 
         elif feature_group == "Location":
+            # Examples:
+            # Cells,Location_CenterMassIntensity_X_DNA
+            # Cells,Location_Center_X
+
             feature_type = parts[2]
             if feature_type == "Center":
                 channel = "None"
@@ -39,24 +51,35 @@ def parse_cp_features(feature):
                 channel = parts[4]
 
         elif feature_group == "Count":
+            # Examples:
+            # Cells,Count,Cells
             feature_type = "None"
             channel = "None"
 
         elif feature_group == "Granularity":
+            # Examples:
+            # Cells,Granularity,15_ER
             feature_type = "None"
             channel = parts[3]
 
         elif feature_group == "Intensity":
+            # Examples:
+            # Cells,Intensity,MeanIntensity_DNA
             feature_type = parts[2]
             channel = parts[3]
 
         elif feature_group == "Correlation":
+            # Examples:
+            # Cells,Correlation,Correlation_DNA_ER
             feature_type = parts[2]
             channel = [parts[3], parts[4]]
             channel.sort()
             channel = "_".join(channel)
 
         elif feature_group in ["Texture", "RadialDistribution"]:
+            # Examples:
+            # Cells,Texture,SumEntropy_ER_3_01_256
+            # Cells,RadialDistribution,FracAtD_mito_tubeness_2of16
             feature_type = parts[2]
             channel = parts[3]
 
