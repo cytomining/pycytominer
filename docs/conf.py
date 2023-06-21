5dc2c2ca294e39bacaa2f753a3475f23b8f45a41
Vincent Rubinetti <vince.rubinetti@gmail.com>
2023-06-21 14:07:24 -0400

    logo touchup

--- docs/conf.py
+++ docs/conf.py
@@ -75,4 +75,4 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ["_static"]
-html_logo = "../logo/just-icon-150px.svg"
+html_logo = "../logo/just-icon.svg"
