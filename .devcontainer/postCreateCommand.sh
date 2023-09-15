74e6c6373e4d6790a0448d4b5ac45aa0f3fd64c3
Erik Serrano <31600622+axiomcura@users.noreply.github.com>
2023-09-15 09:20:22 -0600

    added ipython to rtd requirments file (#309)
    * added ipython to rtd requirments file
    
    * format and typos
    
    * configure RTD to use poetry
    
    * update lock file
    
    * update poetry commands with docs
    
    * specify sphinx ~7.1
    
    * specific with add-ons
    
    ---------
    
    Co-authored-by: Gregory Way <gregory.way@gmail.com>

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 python -m pip install poetry
 poetry config virtualenvs.in-project true --local
-poetry install --all-extras
+poetry install --with cell_locations,collate,dev
 poetry run pre-commit install --install-hooks
