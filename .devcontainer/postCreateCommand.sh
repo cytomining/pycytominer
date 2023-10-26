d5e552b581024264f05463507e8420f0077c996c
Ken Brewer <kenibrewer@users.noreply.github.com>
2023-10-26 17:43:35 +0000

    fix(devcontainer): install poetry via devcontainer feature
    This change will speed up devcontainer loading and resolve some inconsistencies in how vscode treats python environments in the devcontainer
    
    Closes #334

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,4 @@
 #! /usr/bin/env bash
-python -m pip install poetry
 poetry config virtualenvs.in-project true --local
 poetry install --with cell_locations,collate,dev
 poetry run pre-commit install --install-hooks
