848379629dfcad34027283c21308479e659cb3e0
Ken Brewer <kenibrewer@users.noreply.github.com>
2023-10-27 18:38:25 +0000

    feat(build): enable poetry-dyanic-versioning and cz changelog
    This set of changes allows poetry to automatically set the version during build. They also provide a commitizen configuration that allows for automatic changelog generation.
    
    Closes #333

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
+python -m pip install poetry-dynamic-versioning
 poetry config virtualenvs.in-project true --local
 poetry install --with cell_locations,collate,dev
 poetry run pre-commit install --install-hooks
