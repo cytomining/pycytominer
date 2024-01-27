f2946d6126fc32727da1df814144b63f1ea2736c
Ken Brewer <ken@kenbrewer.com>
2024-01-27 15:36:00 +0000

    fix(build): fix build versioning
    This commit fixes an issue with `poetry build` not substituting the dynamic version number. It fixes the issue by adding the optional plugin dependency.
    
    fix #367

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
-python -m pip install poetry-dynamic-versioning
+# PDV will already have been installed in a devcontainer, but is 
+# re-included here for the benefit of manual dev environments.
+poetry self add "poetry-dynamic-versioning[plugin]"
 poetry config virtualenvs.in-project true --local
 poetry install --with dev --all-extras
 poetry run pre-commit install --install-hooks
