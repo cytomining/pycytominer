a357dff8d85bdc83726d64d82c3f21049ca3b3f8
Shantanu Singh <shsingh@broadinstitute.org>
2024-03-23 17:23:41 -0400

    Move command out of postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,8 +1,4 @@
 #! /usr/bin/env bash
-# Specify current directory as safe to avoid the error:
-#  Detected Git repository, but failed because of dubious ownership
-# See https://sam.hooke.me/note/2023/08/poetry-fixing-dubious-ownership-error/
-git config --global --add safe.directory $(pwd)
 # PDV will already have been installed in a devcontainer, but is 
 # re-included here for the benefit of manual dev environments.
 poetry self add "poetry-dynamic-versioning[plugin]"
