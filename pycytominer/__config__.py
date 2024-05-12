5f182d2964bd7b1fdb3665ab7286dda5ee2f57bd
d33bs <dave.bunten@cuanschutz.edu>
2024-05-11 20:00:42 -0600

    fix(pandas): add condition for pandas config

--- pycytominer/__config__.py
+++ pycytominer/__config__.py
@@ -6,4 +6,7 @@ import pandas as pd
 
 # configure pandas copy_on_write for 3.0.0 requirements
 # see: https://pandas.pydata.org/pandas-docs/version/2.2.0/whatsnew/v2.2.0.html#copy-on-write
-pd.options.mode.copy_on_write = True
+# note: we use a conditional here to avoid exceptions
+# with versions of pandas which don't include this option.
+if "copy_on_write" in dir(pd.options.mode):
+    pd.options.mode.copy_on_write = True
