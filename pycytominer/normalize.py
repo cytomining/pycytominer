c452a26dee1aa118a657b96d8fe3e6710c017e35
Erik Serrano <31600622+axiomcura@users.noreply.github.com>
2024-09-27 14:02:55 -0600

    Update pycytominer/normalize.py
    Co-authored-by: Dave Bunten <ekgto445@gmail.com>

--- pycytominer/normalize.py
+++ pycytominer/normalize.py
@@ -34,7 +34,7 @@ def normalize(
     features : list
         A list of strings corresponding to feature measurement column names in the
         `profiles` DataFrame. All features listed must be found in `profiles`.
-        Defaults to "infer". If "infer", then assume CellProfiler features are those
+        Defaults to "infer". If "infer", then assume features are from CellProfiler output and
         prefixed with "Cells", "Nuclei", or "Cytoplasm".
     image_features: bool, default False
         Whether the profiles contain image features.
