4f91fb91a638f9c431bb4808cece1792c7d869e7
Shantanu Singh <shsingh@broadinstitute.org>
2024-12-12 18:17:17 -0500

    Check if downloaded file is empty (#385)
    * Check if downloaded file is empty
    
    * pathlib

--- pycytominer/cyto_utils/cell_locations.py
+++ pycytominer/cyto_utils/cell_locations.py
@@ -177,7 +177,14 @@ class CellLocation:
 
         self.s3.download_file(bucket, key, tmp_file.name)
 
-        return tmp_file.name
+        # Check if the downloaded file exists and has a size greater than 0
+        tmp_file_path = pathlib.Path(tmp_file.name)
+        if tmp_file_path.exists() and tmp_file_path.stat().st_size > 0:
+            return tmp_file.name
+        else:
+            raise ValueError(
+                f"Downloaded file '{tmp_file.name}' is empty or does not exist."
+            )
 
     def _load_metadata(self):
         """Load the metadata into a Pandas DataFrame
